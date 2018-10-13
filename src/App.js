b6bb4759d8ba0a3f0d2b387c4710c618abc980b7
Alejandro Di Leo <ale.dileo17@gmail.com>
2018-10-13 16:04:39 -0300

    Added Sass support

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from "react";
-import "index.scss";
+import "./index.scss";
 
 class App extends Component {
   render() {
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -8,6 +9,14 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ["babel-loader"]
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        loaders: ["file-loader"]
       }
     ]
   },
@@ -19,7 +28,12 @@ module.exports = {
     publicPath: "/",
     filename: "bundle.js"
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: __dirname + "/dist/index.html"
+    })
+  ],
   devServer: {
     contentBase: "./dist",
     hot: true
