076fca0616d489c2ac2c19f10ed7b1fb6dfe7793
Agustin <amulet@ewwoconsulting.com>
2018-10-27 14:16:09 -0300

    Hecho el super login

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,40 @@
 import React from "react";
+import { Button } from "@material-ui/core";
+import { Redirect } from "react-router";
 
-const Login = () => <h1>Login</h1>;
+class Login extends React.Component {
+  state = {
+    name: "",
+    pass: "",
+    redirect: false
+  };
+  handleName = event => {
+    this.setState({ name: event.target.value });
+  };
+  handlePass = event => {
+    this.setState({ pass: event.target.value });
+  };
+  handleForm = () => {
+    if (this.state.name === "equipo5" && this.state.pass === "elmejor") {
+      this.setState({ redirect: true });
+    }
+  };
+  render() {
+    if (this.state.redirect) {
+      return <Redirect push to="/map/" />;
+    }
+    return (
+      <form style={{ maxWidth: "200px" }}>
+        <label>Usuario:</label>
+        <input type="text" onChange={this.handleName} />
+        <label>Contraseña:</label>
+        <input type="password" onChange={this.handlePass} />
+        <Button variant="contained" color="secondary" onClick={this.handleForm}>
+          Log in
+        </Button>
+      </form>
+    );
+  }
+}
 
 export default Login;
